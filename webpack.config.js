81d9bf0f0b41637c76b67ca39113b146c96d04bc
ulrijir <jirik.ulrich@gmail.com>
2021-12-05 14:53:09 +0100

    Cleanup

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './src/jscr/firebase_mod.js',
-  output: {
-    path: path.resolve(__dirname, '_dist/jscr'),
-    filename: 'firebase_mod.js',
-  },
-};
